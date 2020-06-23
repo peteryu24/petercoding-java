06c4448475ad28c30a7d4c839643b3226364ec54
Peter Yu <peteryu24@naver.com>
2020-06-23 19:29:37 +0900

    Submultiples

---
+++ ForSubmultiple.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ForSubmultiple {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Enter a positive number: ");
+		int num = scan.nextInt();
+
+		System.out.println(num + "\'s submultiples are");
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				System.out.print(i +" ");
+			}
+		}
+
+	}
+
+}
