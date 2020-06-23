ff924b8db5dfdf3f4b69c3635f8a6d7b0f080fd6
Peter Yu <peteryu24@naver.com>
2020-06-23 19:33:48 +0900

    Star square
    *******
    *******
    *******
    *******
    *******

---
+++ ForStarSquare.java
@@ -0,0 +1,14 @@
+
+public class ForStarSquare {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 7; j++) {
+				System.out.print("*");
+			}
+			System.out.println(); // change of rows
+		}
+	}
+
+}
