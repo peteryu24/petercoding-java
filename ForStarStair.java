4ce407241ff00c728075eb5f009314f7cd4f0c39
Peter Yu <peteryu24@naver.com>
2020-06-23 19:39:30 +0900

    Star Stair
    *
    **
    ***
    ****
    *****
    ******

---
+++ ForStarStair.java
@@ -0,0 +1,14 @@
+
+public class ForStarStair {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i < 7; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(); // change of rows
+		}
+	}
+
+}
