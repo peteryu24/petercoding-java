f3b3d1cd61cf61b76b429c5471bff516875b939a
Peter Yu <peteryu24@naver.com>
2020-06-23 18:55:04 +0900

    School grades......

---
+++ IfGrades.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class IfGrades {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int num;
+		char grade;
+
+		System.out.print("Enter your score: ");
+		num = scan.nextInt();
+
+		if (num > 90) {
+			grade = 'A';
+		} else if (num > 80) {
+			grade = 'B';
+		} else if (num > 70) {
+			grade = 'C';
+		} else {
+			grade = 'F';
+		}
+
+		System.out.println("Your score is " + grade + ".");
+
+	}
+
+}
