b2f959dd4f1b4f528d67f86a54547126d04ea2c2
Peter Yu <peteryu24@naver.com>
2020-06-23 19:00:27 +0900

    Another school grades......

---
+++ SwitchGrades.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SwitchGrades {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int score, num;
+		char grade;
+
+		System.out.print("Enter your score: ");
+		score = scan.nextInt();
+
+		num = score / 10; // Easier for cases
+
+		switch (num) {
+		case 10:
+		case 9:
+			grade = 'A';
+			break;
+		case 8:
+			grade = 'B';
+			break;
+		case 7:
+			grade = 'C';
+			break;
+		default:
+			grade = 'F';
+			break;
+		}
+
+		System.out.println("Your grade is " + grade + ".");
+	}
+
+}
