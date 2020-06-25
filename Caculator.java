af5e23d075c3ee199c40e631f55e956fe8d2ac77
Peter Yu <peteryu24@naver.com>
2020-06-25 23:58:15 +0900

    Caculator

---
+++ Caculator.java
@@ -0,0 +1,12 @@
+
+public class Caculator {
+
+	public static void sum(int left, int right) {
+		System.out.println(left + right);
+	}
+
+	public static void main(String[] args) {
+		sum(10, 20);
+
+	}
+}
