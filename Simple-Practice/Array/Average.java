5fd5679076579003b61b6f8796604e246477c5d4
Peter Yu <peteryu24@naver.com>
2020-06-26 22:04:56 +0900

    The average for entered numbers.

--- Simple-Practice/Array/Average.java
+++ Simple-Practice/Array/Average.java
@@ -19,7 +19,7 @@ public class ArrayAverage {
 		}
 
 		for (int i = 0; i < arr.length; i++) {
-			sum += arr[i]; // sum = sum = arr[i]
+			sum += arr[i]; //  sum = sum = arr[i]
 		}
 		average = sum / num;
 
