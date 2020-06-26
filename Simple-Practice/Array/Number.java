6498540b80f51eec366eb704fca0531849311d01
Peter Yu <peteryu24@naver.com>
2020-06-26 22:08:37 +0900

    Prints Numbers
    Enter a number: 3
    0 1 2

--- Simple-Practice/Array/Number.java
+++ Simple-Practice/Array/Number.java
@@ -12,7 +12,7 @@ public class ArrayNumber {
 		int[] arr = new int[num];
 
 		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i; // arr[0] = 0, arr[1] = 1, arr[2] =2, ......
+			arr[i] = i; //  arr[0] = 0, arr[1] = 1, arr[2] =2, ......
 		}
 
 		for (int i = 0; i < arr.length; i++) {
