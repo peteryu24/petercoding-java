ab9a200f3958f6042000a0fdc248f9c3e1c6bb8e
Peter Yu <peteryu24@naver.com>
2020-06-26 22:07:14 +0900

    Multiplication
    1 * 1 = 1
    1 * 2 = 2
    1 * 3 = 3
    1 * 4 = 4
    1 * 5 = 5
    1 * 6 = 6
    1 * 7 = 7
    1 * 8 = 8
    1 * 9 = 9

--- Simple-Practice/Array/Multiplication.java
+++ Simple-Practice/Array/Multiplication.java
@@ -5,7 +5,7 @@ public class ArrayMultiplication {
 
 		int[][] arr = new int[10][10];
 
-		for (int i = 1; i < arr.length; i++) { // array starts with 0, but multiplication with 0 is unnecessary
+		for (int i = 1; i < arr.length; i++) { //  array starts with 0, but multiplication with 0 is unnecessary
 			for (int j = 1; j < arr.length; j++) {
 				System.out.println(i + " * " + j + " = " + i * j);
 			}
