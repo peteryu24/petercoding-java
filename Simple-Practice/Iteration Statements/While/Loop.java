809f3e5f3b4e7d2f0ce1f10b5b40fe6b6df0be6f
Peter Yu <peteryu24@naver.com>
2020-06-26 22:14:39 +0900

    Loooooop

--- Simple-Practice/Iteration Statements/While/Loop.java
+++ Simple-Practice/Iteration Statements/While/Loop.java
@@ -9,7 +9,7 @@ public class WhileLoop {
 		System.out.print("Enter the sentence you want to loop: ");
 		String loop = scan.nextLine();
 
-		System.out.print("How many times do you want to loop it? ");
+		System.out.print("How many times do you want to loop it?  ");
 		int times = scan.nextInt();
 
 		int i = 0;
