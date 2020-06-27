15bd558ce6db17ae6f76633f11810b6192851368
Peter Yu <peteryu24@naver.com>
2020-06-27 19:07:34 +0900

    Static, instance - methods/variables

--- Simple-Practice/Object-oriented/Class, Instance, Object/StaticClass.java
+++ Simple-Practice/Object-oriented/Class, Instance, Object/StaticClass.java
@@ -1,22 +1,37 @@
 class Class {
+	static int sta_var = 1;
+	int ins_var = 2; // instance variable
 
-	public static void add(int left, int right) {
-		System.out.println(left + right);
+	static void sta_sta() {
+		System.out.println(sta_var); // static method -> static variable (O)
 	}
 
-	public static void minus(int left, int right) {
-		if (left > right) {
-			System.out.println(left - right);
-		} else {
-			System.out.println(right - left);
-		}
+	static void sta_ins() {
+		// System.out.println(ins_var); // static method -> instance variable (X)
+
+	}
+
+	void ins_sta() {
+		System.out.println(sta_var); // instance method -> static variable (O)
+	}
+
+	void ins_ins() {
+		System.out.println(ins_var); // instance method -> instance variable (O)
 	}
 }
 
 public class StaticClass {
 	public static void main(String[] args) {
+		Class c = new Class();
 
-		Class.add(20, 30);
-		Class.minus(40, 30);
+		c.sta_sta();
+		c.sta_ins(); // can't compile
+		c.ins_sta();
+		c.ins_ins();
+		Class.sta_sta();
+		Class.sta_ins(); // can't compile
+		// Class.ins_sta(); //can't comiple
+		// Class.ins_ins(); //can't compile
+
 	}
 }
