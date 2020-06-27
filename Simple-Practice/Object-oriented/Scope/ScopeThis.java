74e856d7dc8caf9bcb72ce29ac43cb8645f49f62
Peter Yu <peteryu24@naver.com>
2020-06-27 19:57:47 +0900

    Scope with this
    30
    10

---
+++ Simple-Practice/Object-oriented/Scope/ScopeThis.java
@@ -0,0 +1,16 @@
+class Cla {
+	int a = 10;
+
+	public void print() {
+		int a = 30;
+		System.out.println(a); // will print local variable
+		System.out.println(this.a); // will print global varibale
+	}
+}
+
+public class ScopeThis {
+	public static void main(String[] args) {
+		Cla cl = new Cla();
+		cl.print();
+	}
+}
