83b624d5e8b77eb2b42dff748071f44008171306
Peter Yu <peteryu24@naver.com>
2020-07-01 15:49:53 +0900

    Abstract

---
+++ Simple-Practice/Abstract/Abstract.java
@@ -0,0 +1,20 @@
+abstract class Alpha {
+	abstract void a(); // abstract method. Must define by inheriting
+
+	void b() {
+		System.out.println("Hello.");
+	}
+}
+
+class B extends Alpha {
+	void a() {
+		System.out.println("Abstract"); // defined
+	}
+}
+
+public class Abstract {
+	public static void main(String[] args) {
+		B obj = new B();
+		obj.a();
+	}
+}
