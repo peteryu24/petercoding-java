b285aa55fce566d196c24290724e8186265da85a
Peter Yu <peteryu24@naver.com>
2020-07-01 14:00:36 +0900

    OverLoading

---
+++ Simple-Practice/OverRiding/OverLoading.java
@@ -0,0 +1,39 @@
+class OverLoading {
+	int left, middle, right;
+
+	public void setOprands(int left, int right) {
+		this.left = left;
+		this.right = right;
+	}
+
+	public void setOprands(int left, int middle, int right) { // overloading = having a different parameter
+		this.left = left;
+		this.right = right;
+		this.middle = middle;
+	}
+
+	public void sum() {
+		System.out.println(this.left + this.right);
+	}
+
+	public int avg() {
+		return ((this.left + this.right) / 2);
+
+	}
+
+}
+
+class LoadOver extends OverLoading {
+
+	public void sum() {
+		System.out.println("The result is " + (this.left + this.right) + ".");
+	}
+
+	public void substract() {
+		System.out.println(this.left - this.right);
+	}
+
+	public int avg() {
+		return super.avg();
+	}
+}
