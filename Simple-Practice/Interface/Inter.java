5cdf64553059550797184a35767d4f68dcb3b368
Peter Yu <peteryu24@naver.com>
2020-07-01 19:09:50 +0900

    Interface

---
+++ Simple-Practice/Interface/Inter.java
@@ -0,0 +1,25 @@
+public interface Inter { // interface
+	public void setOprands(int first, int second, int third);
+
+	public int sum();
+
+	public int avg();
+}
+
+class Interface implements Inter { // implementing the interface "Interface"
+	int first, second, third;
+
+	public void setOprands(int first, int second, int third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+
+	public int sum() { // must implement all methods [sum(); , avg(); ]
+		return this.first + this.second + this.third;
+	}
+
+	public int avg() {
+		return (this.first + this.second + this.third) / 3;
+	}
+}
