083a279824b2cb4c8746a2285707e477bcc3859b
Peter Yu <peteryu24@naver.com>
2020-07-02 13:10:55 +0900

    Polymorphism Advanced

--- Simple-Practice/Polymorphism/PolymorphismB.java
+++ Simple-Practice/Polymorphism/PolymorphismB.java
@@ -1,19 +1,34 @@
-class A {
-	public String x() {
-		return "x";
-	}
+interface I2 {
+	public String A();
 }
 
-class B extends A {
-	public String y() {
-		return "y";
+interface I3 {
+	public String B();
+}
+
+class D implements I2, I3 {
+	public String A() {
+		return "A";
+	}
+
+	public String B() {
+		return "B";
 	}
 }
 
 public class PolymorphismB {
 	public static void main(String[] args) {
-		A obj = new B();
-		obj.x();
-		// obj.y(); error because class A doesn't hava a 'y' method
+		D obj = new D();
+		I2 objI2 = new D();
+		I3 objI3 = new D();
+
+		obj.A();
+		obj.B();
+
+		objI2.A();
+		// objI2.B(); error , I2 doesn't have a 'B' method
+
+		// objI3.A(); error for the same reason
+		objI3.B();
 	}
 }
