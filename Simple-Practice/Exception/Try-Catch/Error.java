48a928b580956aefee203c6a890ff9d1e04c830b
Peter Yu <peteryu24@naver.com>
2020-07-02 15:32:55 +0900

    Exception Try-Catch
    // Error is : / by zero

---
+++ Simple-Practice/Exception/Try-Catch/Error.java
@@ -0,0 +1,11 @@
+public class Error {
+	public static void main(String[] args) {
+
+		try {
+			System.out.println(10 / 0);
+		} catch (Exception e) {
+			System.out.println("The Error is : " + e.getMessage());
+			// Error is : / by zero
+		}
+	}
+}
