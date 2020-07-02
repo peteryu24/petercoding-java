e5acc3324d2788a5db817f43dd3aa9b7dffae7bd
Peter Yu <peteryu24@naver.com>
2020-07-02 13:29:42 +0900

    PolymorphismC(minimalism)

---
+++ Simple-Practice/Polymorphism/PolymorphismC.java
@@ -0,0 +1,34 @@
+interface father {
+}
+
+interface mother {
+}
+
+interface programmer {
+	public void coding();
+}
+
+interface believer {
+}
+
+class Steve implements father, programmer, believer {
+	public void coding() {
+		System.out.println("fast");
+	}
+}
+
+class Rachel implements mother, programmer {
+	public void coding() {
+		System.out.println("elegance");
+	}
+}
+
+public class PolymorphismC {
+	public static void main(String[] args) {
+		programmer employee1 = new Steve(); // just like minimalism; doesn't have to care about interface father,believer
+		programmer employee2 = new Rachel(); // same for Rachel; no need for mother
+
+		employee1.coding();
+		employee2.coding();
+	}
+}
