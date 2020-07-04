54572a09c49afc8adb609791569690386d83adfc
Peter Yu <peteryu24@naver.com>
2020-07-04 15:27:56 +0900

    Generic Extends

---
+++ Simple-Practice/Generic/GenericExtends.java
@@ -0,0 +1,29 @@
+abstract class Info {
+	public abstract int getNum();
+}
+
+class Information extends Info {
+	public int rank;
+
+	Information(int rank) {
+		this.rank = rank;
+	}
+
+	public int getNum() {
+		return this.rank;
+	}
+}
+
+class Person<T extends Info> { // Restricts the type for Generic
+	public T info;
+
+	Person(T info) {
+		this.info = info;
+	}
+}
+
+public class GenericExtends {
+	public static void main(String[] args) {
+		Person p1 = new Person(new Information(1));
+	}
+}
