3961f132e28282e52e70d25024f4404d857af4d8
Peter Yu <peteryu24@naver.com>
2021-04-22 18:45:58 +0900

    Fibonacci with Recursive
    Enter a number: 5
    3

---
+++ Simple-Practice/Recursive function/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int n= scan.nextInt();
+		System.out.println(fibo(n));
+	}
+	static int fibo(int a) {
+		if(a<=0) {
+			return 0;
+		}else if(a==2){
+			return 1;
+		}else {
+			return(fibo(a-1)+fibo(a-2));
+		}
+	}
+}
